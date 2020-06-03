1fc2897690ec8b8ddd9c9143d949e317655ce861
luysla <dyana.tavares1@gmail.com>
2020-06-03 01:07:11 -0300

    Adicionado listagem de usuário, busca de um único usuário e adicionar usuário

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,32 @@ import express from 'express';
 
 const app = express()
 
+const users = [
+    'Dyana',
+    'Allyson',
+    'Snoopy'
+];
+
+//Busca todos os usuários
 app.get('/users', (req, resp) => {
-    console.log('Listagem de usuários');
+    resp.json(users);
+})
 
-    resp.json([
-        'Dyana',
-        'Allyson',
-        'Snoopy'
-    ]);
+//Busca um único usuário
+app.get('/users/:id', (req, resp) =>{
+    const id = Number(req.params.id);
 
+    return resp.json(users[id])
+})
+
+app.post('/users', (req,resp) => {
+    
+    const user = {
+        'name': 'Dyana',
+        'email': 'dyana.tavares1@gmail.com'
+    };
+
+    return resp.json(user);
 })
 
 app.listen(3333);
